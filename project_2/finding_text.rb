3f889cf1b13c28f6f976b48befa40b0fec7c0200
Martin K <martin.k.io.github@gmail.com>
2020-11-07 23:55:31 +0000

    Update finding text with index search

--- project_2/finding_text.rb
+++ project_2/finding_text.rb
@@ -2,8 +2,17 @@ puts "Please enter some information:"
 
 user_input = gets.chomp
 
-if user_input.include? "name"
-  puts "The user entered their name"
-else
-  puts "The user didn't mention a name"
+user_input_index = user_input.index("name")
+# if user_input.include? "name"
+#   puts "The user entered their name"
+# else
+#   puts "The user didn't mention a name"
+# end
+
+if user_input_index != nil
+  if user_input_index == 0
+    puts "Found it at the beginning!"
+  else
+    puts "Found it somewhere else!"
+  end
 end
