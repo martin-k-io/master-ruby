a1274601fa6b11cd4ccc295c006e5aeb2829bb7d
Martin K <martin.k.io.github@gmail.com>
2020-11-10 22:54:10 +0000

    Add arrays operations

--- project_3/arrays_2.rb
+++ project_3/arrays_2.rb
@@ -2,7 +2,7 @@ words = ["the", "quick", "the", "brown", "fox"]
 
 p words
 
-# Convinience methods (ot syntax sugar)
+# Convenience methods (ot syntax sugar)
 puts words.first
 puts words.last
 
@@ -15,4 +15,5 @@ p words
 p words.take(3)
 
 # Returns an error rather than failing silently(Returning nil)
+# if there's no item with index 8
 puts words.fetch(8)
