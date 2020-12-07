3386376f3f6d48d9a33768920ef706e632da9900
Martin K <martin.k.io.github@gmail.com>
2020-12-07 23:19:14 +0200

    Add unique person_id

--- section_7/person.rb
+++ section_7/person.rb
@@ -3,6 +3,12 @@ require 'csv'
 class Person
   attr_accessor :first_name, :last_name, :person_id
 
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    @person_id  = rand(1000..9999)
+  end
+
   def valid?
     (!first_name.nil? && first_name.length > 0) &&
     (!last_name.nil? && last_name.length > 0)
@@ -21,19 +27,18 @@ class Person
   end
 
   def  save
-    save_file = CSV.open("#{person_id}-file.csv", "w") do |csv|
+    filename = "#{person_id}-file.csv"
+    save_file = CSV.open(filename, "w") do |csv|
       csv << [first_name, last_name]
     end
 
+    puts "File #{filename} saved for employee with ID #{person_id}"
     return !save_file.nil?
   end
 end
 
-person1 = Person.new
+person1 = Person.new("Bob", "Thimpson")
 
-person1.first_name = "Bob"
-person1.last_name = "Thompson"
-person1.person_id = "1001"
 
 #puts person1.valid?
 #p person1.to_csv
