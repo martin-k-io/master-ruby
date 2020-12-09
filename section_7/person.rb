188b11fb0e7385c47c0cff2018be3dd8f7d51bf2
Martin K <martin.k.io.github@gmail.com>
2020-12-09 15:23:42 +0200

    Add update method

--- section_7/person.rb
+++ section_7/person.rb
@@ -1,23 +1,23 @@
 require 'csv'
 
 class Person
-  attr_accessor :first_name, :last_name, :person_id
+  attr_accessor :first_name, :last_name
+  attr_accessor :person_id, :filename
 
   def initialize(first_name, last_name, person_id = rand(1000..9999))
     @first_name = first_name
     @last_name  = last_name
     @person_id  = person_id
+    @filename = "#{person_id}-file.csv"
+  end
+
+  def self.filename(person_id)
+    "#{person_id}-file.csv"
   end
 
   def valid?
     (!first_name.nil? && first_name.length > 0) &&
     (!last_name.nil? && last_name.length > 0)
-  end
-
-  def walk
-  end
-
-  def talk
   end
 
   def to_csv
@@ -28,8 +28,8 @@ class Person
 
   # class method
   def self.read(person_id)
-    filename = "#{person_id}-file.csv"
-
+    filename = Person.filename(person_id)
+    
     if File.exist?(filename)
       File.open(filename, 'r') do |file|
         record = CSV.parse(file.read)[0]
@@ -40,15 +40,38 @@ class Person
     end
   end
 
+  def create
+    if !File.exist?(filename)
+      save
+    else
+      puts "Cannot create file. File #{filename} already exists for #{person_id}"
+      return false
+    end
+  end
+
+  def update
+    if File.exist?(filename)
+      save
+      puts "File #{filename} updated for employee with ID #{person_id}"
+    else
+      puts "File does not exist, update cannot be performed"
+      return false
+    end
+  end
+
   # instance method
   def  save
-    filename = "#{person_id}-file.csv"
-
     save_file = CSV.open(filename, 'w') do |csv|
       csv << [first_name, last_name]
     end
 
     puts "File #{filename} saved for employee with ID #{person_id}"
     return !save_file.nil?
+  end
+
+  def walk
+  end
+
+  def talk
   end
 end
--- section_7/runner.rb
+++ section_7/runner.rb
@@ -1,10 +1,22 @@
 require_relative "person.rb"
 
-person1 = Person.new("Bob", "Thimpson")
+#person1 = Person.new("Bob", "Thimpson")
 
 #puts person1.valid?
 #p person1.to_csv
 
-puts person1.save
+#puts person1.save
 
-p Person.read(9643)
+#p Person.read(9643)
+
+#person = Person.read(9624)
+#person.last_name = "Gordon"
+
+#puts person.update
+
+#person = Person.new("Lisa", "Jones")
+
+#p person.create
+person = Person.read(4335)
+person.last_name = "Roberts"
+p person.update
