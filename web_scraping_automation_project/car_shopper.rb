3738e96d7fcf19f5044b061cd7beca31ba6d4b6c
Martin K <martin.k.io.github@gmail.com>
2020-12-10 13:37:14 +0200

    Convert price into an integer

--- web_scraping_automation_project/car_shopper.rb
+++ web_scraping_automation_project/car_shopper.rb
@@ -2,6 +2,10 @@ require 'httparty'
 require 'nokogiri'
 require 'awesome_print'
 
+def convert_price(price)
+  price.gsub('$', '').gsub(',','').to_i
+end
+
 def car_shopper
   response = HTTParty.get('https://code.evgenyrahman.com/rubycourse/carlist.html')
   parsed_html = Nokogiri::HTML(response.body)
@@ -14,8 +18,8 @@ def car_shopper
     {
       make: each_car.css('.make').text,
       year: each_car.css('.year').text,
-      price: each_car.css('.price').text,
-      rating: each_car.css('.star.rating').attribute('data-rating').value
+      price: convert_price(each_car.css('.price').text),
+      rating: each_car.css('.star.rating').attribute('data-rating').value.to_i
     }
   end
 
@@ -24,6 +28,8 @@ def car_shopper
   File.open('car_listings.json', 'wb') do |f|
     f << cars.to_json
   end
+
+  puts "Results were written to file!"
 end
 
 car_shopper
