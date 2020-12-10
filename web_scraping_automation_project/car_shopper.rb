844926444672d123cd7056a4d0029cd5666e64c4
Martin K <martin.k.io.github@gmail.com>
2020-12-10 13:03:42 +0200

    Extract data into a hash

--- web_scraping_automation_project/car_shopper.rb
+++ web_scraping_automation_project/car_shopper.rb
@@ -8,19 +8,18 @@ def car_shopper
 
   # Returns a collection of all card car classes
   car_listings = parsed_html.css('.card.car')
-
-  pp car_listings.first
+  #pp car_listings.first
 
   car_listings.each do |each_car|
-    pp each_car.css('.make').text
+    car = {
+      make: each_car.css('.make').text,
+      year: each_car.css('.year').text,
+      price: each_car.css('.price').text,
+      rating: each_car.css('.star.rating').attribute("data-rating").value
+    }
+
+    pp car
   end
-
-
-
-
-
-  puts "Welcome to the card shopper!"
-
 end
 
 car_shopper
