0d0f6a3fa8ed177cb4f3f17546943c551b6da1e9
Martin K <martin.k.io.github@gmail.com>
2020-12-10 16:50:47 +0200

    Sanitizing data

--- data_engineering/log_parser.rb
+++ data_engineering/log_parser.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 def parse_log
   filename = 'data/access_log_20190521-211058.log'
 
@@ -32,5 +34,27 @@ def extract_email(log_line)
   email = log_line.match(%r{signup\?email=([a-zA-Z0-9@.]*) HTTP/})
   email.captures.first
 end
+
+def cross_reference(_log_line)
+  users = CSV.open('data/users.csv') do |csv|
+    csv.readlines
+  end
+
+  users.map! do |line|
+    if line.length == 3
+      line[0] = 'Unknown' if line[0].nil?
+      line[1] = 'Unknown' if line[1].nil?
+    elsif line[0].match(/[a-zA-Z0-9@.]*/)
+      email = line[0]
+      line[0] = 'Unknown'
+      line.push('Unknown')
+      line.push(email)
+    end
+
+    line
+  end
+end
+
+cross_reference('')
 
 pp parse_log
