a6bf6d63d61c99aced7f254d0e0463687cb889b5
Martin K <martin.k.io.github@gmail.com>
2020-12-10 16:35:20 +0200

    Extract browser and email address

--- data_engineering/log_parser.rb
+++ data_engineering/log_parser.rb
@@ -1,7 +1,36 @@
-filename = 'data/access_log_20190521-211058.log'
+def parse_log
+  filename = 'data/access_log_20190521-211058.log'
 
-access_log = File.open(filename,'r') do |f|
-  f.read
+  access_log_lines = File.open(filename, 'r') do |f|
+    f.readlines
+  end
+
+  signup_log_lines = access_log_lines.select do |line|
+    line.include?('/signup?email=')
+  end
+
+  user_data = signup_log_lines.map do |line|
+    parsed_array = line.split('" "')
+    email = extract_email(parsed_array.first)
+    user_agent_string = parsed_array.last
+    browser = determine_browser(user_agent_string)
+    [email, browser]
+  end
+
+  # puts user_data
 end
 
-puts access_log.length
+def determine_browser(user_agent)
+  return 'Firefox' if user_agent.include?('Firefox') && user_agent.include?('Gecko')
+  return 'Chrome' if user_agent.include?('Chrome')
+  return 'Safari' if user_agent.include?('Gecko') && user_agent.include?('Safari')
+
+  'Other'
+end
+
+def extract_email(log_line)
+  email = log_line.match(%r{signup\?email=([a-zA-Z0-9@.]*) HTTP/})
+  email.captures.first
+end
+
+pp parse_log
